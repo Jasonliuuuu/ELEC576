40713c09d7eb0bd2daaa7b22e78670b14c4dae46
Jasonliuuuu <jason890418123@gmail.com>
2024-09-10 17:41:17 -0500

    ELEC576 Assignment 0 Task 6

---
+++ test.py
@@ -0,0 +1,15 @@
+# Function to convert Fahrenheit to Celsius
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (5 / 9) * (fahrenheit - 32)
+    return celsius
+
+
+fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+
+# Call the conversion function
+celsius = fahrenheit_to_celsius(fahrenheit)
+
+# Output the Celsius temperature
+print(f"{fahrenheit} Fahrenheit is equal to {celsius:.2f} Celsius")
